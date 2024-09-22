7bc1fbd5f1b6b8530e6019aa5cafb1d15cc8b1bf
Pgr18 <paulry@mail.ru>
2024-09-22 20:22:43 +0300

    Trying to get some try/catch

--- tokio_app/src/main.rs
+++ tokio_app/src/main.rs
@@ -1,8 +1,22 @@
 //use mini_redis::{client,Result};
 pub mod serial;
 
+use std::any::type_name;
+
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 
 fn main()  {
-    let _= serial::main();
+    let _= serial::scan();
+    let _ok_port= serial::connect("COM3");
+    if type_of(_ok_port) == "SerialPort" {
+        println!("OK");
+    } else {
+        println!("Error");
+    }
+
+    
 }
--- tokio_app/src/serial.rs
+++ tokio_app/src/serial.rs
@@ -1,10 +1,16 @@
 use serial2_tokio::SerialPort;
+use std::io::Error;
+
 #[tokio::main]
-pub async fn main() -> Result<(),()> {
-// On Windows, use something like "COM1" or "COM15".
+pub async fn scan() -> Result<(),()> {
 let ports = SerialPort::available_ports().expect("No ports found!");
 for p in ports {
     println!("{}", p.display().to_string());
 }
 Ok(())
 }
+
+pub async fn connect(user_path : &str) -> Result<SerialPort,Error> {
+    let port = SerialPort::open(user_path, 921600)?;
+    Ok(port)
+    }
